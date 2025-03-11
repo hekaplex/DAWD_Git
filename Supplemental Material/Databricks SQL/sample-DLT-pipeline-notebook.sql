936b9bb784efa9a3f56549363e73ff290aa63a86
hekaplex <hekaplex>
2025-03-11 16:19:46 +0000

    dlt sample

---
+++ Supplemental Material/Databricks SQL/sample-DLT-pipeline-notebook.sql
@@ -0,0 +1,37 @@
+-- Databricks notebook source
+USE hive_metastore.labuser6737320_1741615056_30pd_da_adewd_4_1
+
+-- COMMAND ----------
+
+CREATE
+OR REFRESH STREAMING TABLE taxi_raw_records (
+  CONSTRAINT valid_distance EXPECT (trip_distance > 0.0) ON VIOLATION DROP ROW
+) AS
+SELECT
+  *
+FROM
+  STREAM(samples.nyctaxi.trips)
+
+-- COMMAND ----------
+
+CREATE
+OR REFRESH MATERIALIZED VIEW total_fare_amount_by_week AS
+SELECT
+  date_trunc("week", tpep_pickup_datetime) as week,
+  SUM(fare_amount) as total_amount
+FROM
+  live.taxi_raw_records
+GROUP BY
+  week
+
+-- COMMAND ----------
+
+CREATE
+OR REFRESH MATERIALIZED VIEW max_distance_by_week AS
+SELECT
+  date_trunc("week", tpep_pickup_datetime) as week,
+  MAX(trip_distance) as max_distance
+FROM
+  live.taxi_raw_records
+GROUP BY
+  week
